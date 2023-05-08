4abca12a8f702f752b0b64a31ae6033a75b77bd7
Sudha2022Test <109454681+Sudha2022Test@users.noreply.github.com>
2023-05-08 12:52:35 +0530

    Update Wrappers.java

--- src/main/java/wrappers/Wrappers.java
+++ src/main/java/wrappers/Wrappers.java
@@ -5,7 +5,6 @@ public interface Wrappers {
 	/**
 	 * This method will launch the given browser and maximise the browser and set the
 	 * wait for 30 seconds and load the url
-	 * @author Basha - LibertyTestingCenter
 	 * @param browser - The browser of the application to be launched
 	 * @param url - The url with http or https
 	 * @throws Exception 
@@ -18,7 +17,6 @@ public interface Wrappers {
 	 * 
 	 * @param idValue - id of the webelement
 	 * @param data - The data to be sent to the webelement
-	 * @author Basha - LibertyTestingCenter
 	 * @return 
 	 * @throws Exception 
 	 */
@@ -29,7 +27,6 @@ public interface Wrappers {
 	 * 
 	 * @param nameValue - name of the webelement
 	 * @param data - The data to be sent to the webelement
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void enterByName(String nameValue, String data) ;	
 	
@@ -38,7 +35,6 @@ public interface Wrappers {
 	 * 
 	 * @param xpathValue - name of the webelement
 	 * @param data - The data to be sent to the webelement
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void enterByXpath(String xpathValue, String data);
 
@@ -46,7 +42,6 @@ public interface Wrappers {
 	/**
 	 * This method will verify the title of the browser 
 	 * @param title - The expected title of the browser
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void verifyTitle(String title);
 	
@@ -54,7 +49,6 @@ public interface Wrappers {
 	 * This method will verify the given text
 	 * @param id - The locator of the object in id
 	 * @param text  - The text to be verified
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void verifyTextById(String id, String text);
 	
@@ -62,7 +56,6 @@ public interface Wrappers {
 	 * This method will verify the given text with exact match
 	 * @param xpath - The locator of the object in id
 	 * @param text  - The text to be verified
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void verifyTextByXpath(String xpath, String text);
 	
@@ -70,7 +63,6 @@ public interface Wrappers {
 	 * This method will verify the given text with partial match
 	 * @param xpath - The locator of the object in xpath
 	 * @param text  - The text to be verified
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void verifyTextContainsByXpath(String xpath, String text);
 
@@ -78,14 +70,12 @@ public interface Wrappers {
 	/**
 	 * This method will click the element using id as locator
 	 * @param id  The id (locator) of the element to be clicked
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void clickById(String id) ;
 
 	/**
 	 * This method will click the element using ClassName as locator
 	 * @param class  The class (locator) of the element to be clicked
-	 * @author Basha - LibertyTestingCenter
 	 * @throws Exception 
 	 */
 	public void clickByClassName(String classVal);
@@ -93,7 +83,6 @@ public interface Wrappers {
 	/**
 	 * This method will click the element using name as locator
 	 * @param name  The name (locator) of the element to be clicked
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void clickByName(String name) ;
 	
@@ -101,42 +90,36 @@ public interface Wrappers {
 	/**
 	 * This method will click the element using link name as locator and do take snap
 	 * @param name  The link name (locator) of the element to be clicked
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void clickByLink(String name);
 	
 	/**
 	 * This method will click the element using link name as locator and do not take snap
 	 * @param name  The link name (locator) of the element to be clicked
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void clickByLinkNoSnap(String name);
 
 	/**
 	 * This method will click the element using xpath as locator
 	 * @param xpathVal  The xpath (locator) of the element to be clicked
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void clickByXpath(String xpathVal);
 	
 	/**
 	 * This method will click the element using xpath as locator
 	 * @param xpathVal  The xpath (locator) of the element to be clicked
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void clickByXpathNoSnap(String xpathVal);
 	
 	/**
 	 * This method will get the text of the element using id as locator
 	 * @param xpathVal  The id (locator) of the element 
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public String getTextById(String idVal);
 
 	/**
 	 * This method will get the text of the element using xpath as locator
 	 * @param xpathVal  The xpath (locator) of the element 
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public String getTextByXpath(String xpathVal);
 
@@ -144,7 +127,6 @@ public interface Wrappers {
 	 * This method will select the drop down visible text using id as locator
 	 * @param id The id (locator) of the drop down element
 	 * @param value The value to be selected (visibletext) from the dropdown 
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void selectVisibileTextById(String id, String value);
 	
@@ -152,37 +134,31 @@ public interface Wrappers {
 	 * This method will select the drop down using index as id locator
 	 * @param id The id (locator) of the drop down element
 	 * @param value The value to be selected (visibletext) from the dropdown 
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void selectIndexById(String id, int value);
 	
 	/**
 	 * This method will switch to the parent Window
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void switchToParentWindow();
 	
 	/**
 	 * This method will move the control to the last window
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void switchToLastWindow();
 	
 	/**
 	 * This method will accept the alert opened
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void acceptAlert();
 	
 	/**
 	 * This method will dismiss the alert opened
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void dismissAlert();
 	
 	/**
 	 * This method will return the text of the alert
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public String getAlertText();
 	
@@ -190,14 +166,12 @@ public interface Wrappers {
 		
 	/**
 	 * This method will close the active browser
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void closeBrowser();
 	
 	
 	/**
 	 * This method will close all the browsers
-	 * @author Basha - LibertyTestingCenter
 	 */
 	public void closeAllBrowsers();
 	
